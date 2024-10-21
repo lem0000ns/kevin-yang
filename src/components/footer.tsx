fe55805403deeeb2c4682d865f4ddd641ccb9378
kevinyang.poke@gmail.com <kevinyang.poke@gmail.com>
2024-10-20 18:10:41 -0700

    modified footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -6,10 +6,10 @@ const footer = () => {
     <div>
       <hr className="border-t border-purple my-8 w-2/3 mx-auto" />
       <div className="flex lg:w-1/2 w-3/4 justify-between items-center mx-auto">
-        <div>
+        <div className="lg:block hidden">
           Designed by <strong>Kevin Yang</strong>
         </div>
-        <div className="flex space-x-4">
+        <div className="flex space-x-4 lg:mx-0 mx-auto">
           <a
             href="https://www.instagram.com/kevonsheep/"
             target="_blank"
